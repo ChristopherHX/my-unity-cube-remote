498b5a5436674eaaa2d29f7defdc2d04d1148606
Christopher Homberger <christopher.homberger@web.de>
2024-05-24 21:02:19 +0200

    Initial

---
+++ app/src/main/java/de/homberger/myunityremote/MainActivity.java
@@ -0,0 +1,94 @@
+package de.homberger.myunityremote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private float x, y, z;
+    private float gx, gy, gz;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private static int writeFloat(byte[] frame, int offset, float v) {
+        int iv = Float.floatToIntBits(v);
+        frame[offset] = (byte) (iv >> 24);
+        frame[offset + 1] = (byte) (iv >> 16);
+        frame[offset + 2] = (byte) (iv >> 8);
+        frame[offset + 3] = (byte) (iv);
+        return offset + 4;
+    }
+
+    public void onConnect(View view) {
+        EditText hostTextEdit = findViewById(R.id.hostTextEdit);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String ip = hostTextEdit.getText().toString();
+                    InetAddress raw = InetAddress.getByName(ip);
+                    Socket sock = new Socket(raw, 8904);
+                    OutputStream os = sock.getOutputStream();
+                    while(true) {
+                        byte[] frame = new byte[4 * 6];
+                        int off = 0;
+                        off = writeFloat(frame, off, x);
+                        off = writeFloat(frame, off, y);
+                        off = writeFloat(frame, off, z);
+                        off = writeFloat(frame, off, gx);
+                        off = writeFloat(frame, off, gy);
+                        off = writeFloat(frame, off, gz);
+                        os.write(frame);
+                        os.flush();
+                        Thread.sleep(100);
+                    }
+                } catch (Exception e) {
+                    return;
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            x = sensorEvent.values[0];
+            y = sensorEvent.values[1];
+            z = sensorEvent.values[2];
+        }
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            gx = sensorEvent.values[0];
+            gy = sensorEvent.values[1];
+            gz = sensorEvent.values[2];
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
