e5c803e72acf8c9f05c298c4fca19dc2f0e73e10
Christopher Homberger <christopher.homberger@web.de>
2024-07-13 12:07:47 +0200

    Positions Experiments

--- app/src/main/java/de/homberger/myunityremote/MainActivity.java
+++ app/src/main/java/de/homberger/myunityremote/MainActivity.java
@@ -1,8 +1,15 @@
 package de.homberger.myunityremote;
 
+import static java.lang.Math.acos;
+import static java.lang.Math.atan;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
+import android.graphics.Matrix;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,12 +18,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.security.Timestamp;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -24,13 +37,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float x, y, z;
     private float gx, gy, gz;
 
+    private float[] speed = new float[3];
+    private float[] position = new float[3];
+    private long last, glast;
+    private TextView textView;
+    private float[] angle = new float[3];
+    private float[] aAngle = new float[3];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+
+        textView = findViewById(R.id.textView);
+
+
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     private static int writeFloat(byte[] frame, int offset, float v) {
@@ -41,7 +66,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         frame[offset + 3] = (byte) (iv);
         return offset + 4;
     }
+    public void reset(View view) {
+        speed[0] = 0;
+        speed[1] = 0;
+        speed[2] = 0;
+        position[0] = 0;
+        position[1] = 0;
+        position[2] = 0;
 
+        angle[0] = 0;
+        angle[1] = 0;
+        angle[2] = 0;
+    }
     public void onConnect(View view) {
         EditText hostTextEdit = findViewById(R.id.hostTextEdit);
         Thread thread = new Thread() {
@@ -54,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     Socket sock = new Socket(raw, Integer.parseInt(ip.substring(p + 1)));
                     OutputStream os = sock.getOutputStream();
                     while(true) {
-                        byte[] frame = new byte[4 * 6];
+                        byte[] frame = new byte[4 * 9];
                         int off = 0;
                         off = writeFloat(frame, off, x);
                         off = writeFloat(frame, off, y);
@@ -62,9 +98,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                         off = writeFloat(frame, off, gx);
                         off = writeFloat(frame, off, gy);
                         off = writeFloat(frame, off, gz);
+                        off = writeFloat(frame, off, gx);
+                        off = writeFloat(frame, off, gy);
+                        off = writeFloat(frame, off, gz);
                         os.write(frame);
                         os.flush();
-                        Thread.sleep(100);
+//                        Thread.sleep(100);
                     }
                 } catch (Exception e) {
                     return;
@@ -74,17 +113,128 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         thread.start();
     }
 
+
+    Matrix getRz(float a) {
+        Matrix m = new Matrix();
+        m.setValues(new float[] { (float) cos(a), (float) -sin(a), 0, (float) sin(a), (float) cos(a), 0, 0, 0, 1 });
+        return m;
+    }
+
+    Matrix getRx(float a) {
+        Matrix m = new Matrix();
+        m.setValues(new float[] { 1, 0, 0, 0, (float) cos(a), (float) -sin(a), 0, (float) sin(a), (float) cos(a) });
+        return m;
+    }
+
+    Matrix getRy(float a) {
+        Matrix m = new Matrix();
+        m.setValues(new float[]{(float) cos(a), 0, (float) sin(a), 0, 1, 0, (float) -sin(a), 0, (float) cos(a)});
+        return m;
+    }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        long cur = System.nanoTime();
         if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            float delta = (cur - last) / 1000000000.0f;
+            if(last == 0) {
+                last = cur;
+                return;
+            }
+            last = cur;
+
+//            Matrix rx = new Matrix();
+//            rx.setValues(new float[] { (float) cos(angle[0]), (float) -sin(angle[0]), 0, (float) sin(angle[0]), (float) cos(angle[0]), 0, 0, 0, 1 });
+//            Matrix ry = new Matrix();
+//            ry.setValues(new float[] { 1, 0, 0, 0, (float) cos(angle[1]), (float) -sin(angle[1]), 0, (float) sin(angle[1]), (float) cos(angle[1]) });
+//
+//            Matrix rz = new Matrix();
+//            rz.setValues(new float[] { (float) cos(angle[2]), (float) -sin(angle[2]), 0, 1, 0, 0, (float) sin(angle[2]), (float) cos(angle[2]) });
+            //m.preRotate(3.14f);
+
             x = sensorEvent.values[0];
             y = sensorEvent.values[1];
             z = sensorEvent.values[2];
+
+
+
+            float gravity = (float) sqrt(x * x + y * y + z * z);
+            if(gravity >= 9.80 && gravity < 9.82) {
+                aAngle[0] = (float) atan(y / z);
+                aAngle[1] = (float) -atan(x / z);
+                aAngle[2] = 0;
+
+                angle[0] = aAngle[0];
+                angle[1] = aAngle[1];
+                angle[2] = aAngle[2];
+
+                speed[0] = 0;
+                speed[1] = 0;
+                speed[2] = 0;
+
+            } else {
+
+                //m.setValues(new float[] {  });
+
+//                speed[0] += x * delta;
+//                speed[1] += y * delta;
+//                speed[2] += z * delta;
+//
+//                position[0] += 100 * speed[0] * delta;
+//                position[1] += 100 * speed[1] * delta;
+//                position[2] += 100 * speed[2] * delta;
+            }
+
+            Matrix rotate = new Matrix();
+            //rotate.preConcat(getRz(0));
+            rotate.preConcat(getRx(-angle[0]));
+            rotate.preConcat(getRy(-angle[1]));
+            float[] in = new float[]{0, 0, 9.81f};
+            float[] xyz = new float[]{0,0,0};
+
+            //rotate.mapVectors(xyz, in);
+            Matrix res = new Matrix();
+            float[] mres = new float[]{0, 0, 0, 0, 0, 0, 9.81f, 0, 0};
+            res.setValues(mres);
+            res.postConcat(rotate);
+            res.getValues(mres);
+            xyz[0] = mres[0];
+            xyz[1] = mres[3];
+            xyz[2] = mres[6];
+            float[] lxyz = new float[]{0,0,0};
+            lxyz[0] = x - mres[0];
+            lxyz[1] = y - mres[3];
+            lxyz[2] = z - mres[6];
+
+            speed[0] += lxyz[0] * delta;
+            speed[1] += lxyz[1] * delta;
+            speed[2] += lxyz[2] * delta;
+
+            position[0] += 100 * speed[0] * delta;
+            position[1] += 100 * speed[1] * delta;
+            position[2] += 100 * speed[2] * delta;
+
+
+            textView.setText("delta: " + delta + "\naccel:\n" + Arrays.toString(sensorEvent.values).replace(", ", ", \n" ) + "\naccel rot:\n" + Arrays.toString(xyz).replace(", ", ", \n" ) +"\naccel lin:\n" + Arrays.toString(lxyz).replace(", ", ", \n" ) + "\nspeed: \n" + Arrays.toString(speed).replace(", ", ",\n" ) + "\nposition: \n" + Arrays.toString(position).replace(", ", ",\n") + "\ngx: " + gx + "\ngy: " + gy + "\ngz: " + gz + "\nangle:\n" + Arrays.toString(angle).replace(", ", ", \n" ) + "\naAngle:\n" + Arrays.toString(aAngle).replace(", ", ", \n" ));
+            //Matrix proc = new Matrix();
+            //android.opengl.Matrix.multiplyMM();
         }
         if(sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
             gx = sensorEvent.values[0];
             gy = sensorEvent.values[1];
             gz = sensorEvent.values[2];
+            double delta = (cur - glast) / 1000000000.0f;
+
+            if(glast == 0) {
+                glast = cur;
+                return;
+            }
+            glast = cur;
+
+            angle[0] += gx * delta;
+            angle[1] += gy * delta;
+            angle[2] += gz * delta;
         }
     }
 
